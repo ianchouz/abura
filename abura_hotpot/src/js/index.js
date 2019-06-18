d064cbcb896d358f338f999720bd7c504a00953b
ianchou <rhythm884@gmail.com>
2019-06-18 20:10:41 +0800

    2019/6/18 s2 row1

--- abura_hotpot/src/js/index.js
+++ abura_hotpot/src/js/index.js
@@ -1,9 +1,37 @@
 // vue
 const vm = new Vue({
-  delimiters: ["<%", "%>"],
-  el: "#mainContainer",
-  data: { s1Data }
+  delimiters: ['<%', '%>'],
+  el: '#mainContainer',
+  data: { s1Data, s2Data }
 });
 
+// s1
 // swiper
-const s1Swiper = new Swiper(".s1_swiper-container", {});
+const s1Swiper = new Swiper('.s1__swiper-container', {
+  // Disable preloading of all images
+  preloadImages: false,
+  lazy: {
+    loadPrevNext: true
+  },
+  pagination: {
+    el: '.s1__swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
+// s2
+// row1
+// swiper
+const s2_1Swiper = new Swiper('.s2-1__swiper-container', {
+  // Disable preloading of all images
+  preloadImages: false,
+  lazy: {
+    loadPrevNext: true
+  },
+  pagination: {
+    el: '.s2-1__swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
