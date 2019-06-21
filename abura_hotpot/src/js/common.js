400d78079b45ab8e4b07f189463f43c69e81fb8c
ianchou <rhythm884@gmail.com>
2019-06-21 18:39:28 +0800

    2019 6/21 dsk function ok

--- abura_hotpot/src/js/common.js
+++ abura_hotpot/src/js/common.js
@@ -7,35 +7,44 @@ const $id = function(id) {
   return document.getElementById(id);
 };
 
-// scrollToTop
-const scrollToTop = function() {
-  const t = document.documentElement.scrollTop || document.body.scrollTop;
-  if (t > 0) {
-    window.scrollTo(0, t - t / 8);
-    window.requestAnimationFrame(scrollToTop);
+const vueMixins = {
+  delimiters: ['<%', '%>'],
+  data: {
+    targetOffsetTop: 0
+  },
+  methods: {
+    showNavMenu() {
+      $id('commonHeader').classList.toggle('nav-menu-open');
+    },
+    scrollToTop() {
+      const t = document.documentElement.scrollTop || document.body.scrollTop;
+      if (t > 0) {
+        window.scrollTo(0, t - t / 8);
+        window.requestAnimationFrame(this.scrollToTop);
+      }
+    },
+    scrollToNext() {
+      const t = document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (t < $id('scrollDownAnchor').offsetTop) {
+        window.scrollTo(0, t + Math.ceil(($id('scrollDownAnchor').offsetTop - t) / 8));
+        window.requestAnimationFrame(this.scrollToNext);
+      }
+    },
+    setTargetOffsetTop(target) {
+      this.targetOffsetTop = $id(target).getBoundingClientRect().top;
+      // console.log(this.targetOffsetTop);
+
+      this.goToSection();
+    },
+    goToSection() {
+      const t = document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (this.targetOffsetTop !== 0) {
+        window.scrollTo(0, t + Math.ceil(this.targetOffsetTop / 8));
+        this.targetOffsetTop -= Math.ceil(this.targetOffsetTop / 8);
+        window.requestAnimationFrame(this.goToSection);
+      }
+    }
   }
 };
-
-// scrollDown
-const scrollToNext = function() {
-  const t = document.documentElement.scrollTop || document.body.scrollTop;
-
-  if (t < $id('scrollDownAnchor').offsetTop) {
-    window.scrollTo(0, t + Math.ceil(($id('scrollDownAnchor').offsetTop - t) / 8));
-    window.requestAnimationFrame(scrollToNext);
-  }
-};
-
-window.addEventListener('load', function() {
-  $id('goTopBtn').addEventListener('click', function() {
-    scrollToTop();
-  });
-
-  $id('scrollDownBtn').addEventListener('click', function() {
-    scrollToNext();
-  });
-
-  $id('menuToggle').addEventListener('click', function() {
-    $id('commonHeader').classList.toggle('nav-menu-open');
-  });
-});
--- abura_hotpot/src/js/index.js
+++ abura_hotpot/src/js/index.js
@@ -1,6 +1,5 @@
 // vue
 const vm = new Vue({
-  delimiters: ['<%', '%>'],
   el: '#mainContainer',
   data: {
     footerData,
@@ -10,26 +9,40 @@ const vm = new Vue({
     s2row3SwiperIndex: 0,
     s3Data,
     s4Data,
-    s5Data
+    s5Data,
+    newsIndex: 0
   },
+  mixins: [vueMixins],
   methods: {
     hoverChangeImg(index) {
       // console.log(index);
       this.s2row2ImgIndex = index;
     },
     hoverChangeSet(e, index) {
+      // console.log(index);
       this.s2row3SwiperIndex = index;
 
       setTimeout(() => {
         vm.setTargetLeft();
       }, 0);
     },
+    hoverChangeNewsIndex(index) {
+      // console.log(index);
+      this.newsIndex = index;
+    },
     setTargetLeft() {
       const targetLeft = document.querySelector('.set-item.active').getBoundingClientRect().left;
       $id('s2_3BorderDecoration').style.left = `${targetLeft}px`;
     },
-    showPopup() {
-      $id('menuPopup').classList.add('active');
+    showPopup(popupType) {
+      // console.log(popupType);
+      if (popupType === 'menu') {
+        $id('menuPopup').classList.add('active');
+      } else if (popupType === 'set') {
+        $id('setPopup').classList.add('active');
+      } else if (popupType === 'news') {
+        $id('newsPopup').classList.add('active');
+      }
     }
   },
   mounted() {
@@ -87,6 +100,7 @@ const s2_3Swiper = new Swiper('.s2-3__swiper-container', {
 
 // s3
 // swiper
+let pageClockOffsetTop = 0;
 const s3Swiper = new Swiper('.s3__swiper-container', {
   // Disable preloading of all images
   preloadImages: false,
@@ -97,6 +111,18 @@ const s3Swiper = new Swiper('.s3__swiper-container', {
     el: '.s3__swiper-pagination',
     type: 'bullets',
     clickable: true
+  },
+  on: {
+    init: function() {
+      // console.log('swiper initialized');
+      pageClockOffsetTop = $id('pageClock').children[this.activeIndex].offsetTop;
+      $id('pageClock').style.transform = `translateY(-${pageClockOffsetTop}px)`;
+    },
+    slideChange: function() {
+      // console.log('slideChange');
+      pageClockOffsetTop = $id('pageClock').children[this.activeIndex].offsetTop;
+      $id('pageClock').style.transform = `translateY(-${pageClockOffsetTop}px)`;
+    }
   }
 });
 
